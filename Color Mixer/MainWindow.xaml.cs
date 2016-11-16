5f79d7d917c1000d2297b4409c727e6bf3850597
Neville <nevil_dabre@yahoo.com>
2016-11-16 11:52:31 -0500

    Add project files.

---
+++ Color Mixer/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿/*Assignment#3 Group#6
+ * Aicun Lu
+ * Neville Francis Dabre
+ * Abhimanyu Vats
+ * Angileena Jacob 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Color_Mixer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MixVM mvm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mvm = new MixVM();
+            DataContext = mvm;
+        }
+    }
+}
---
+++ Color Mixer/MixVM.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Color_Mixer
+{
+    class MixVM : INotifyPropertyChanged
+    {
+        private double redColor;
+        private double greenColor;
+        private double blueColor;
+        private string backgroundColor;
+    
+        public double RedColor {
+            get { return redColor; }
+            set {
+                redColor = value;
+                changeBackground();
+                OnPropertyChanged();
+            }
+        }
+
+        public double GreenColor
+        {
+            get { return greenColor; }
+            set {
+                greenColor = value;
+                changeBackground();
+                OnPropertyChanged();
+            }
+        }
+
+        public double BlueColor {
+            get { return blueColor; }
+            set {
+                blueColor = value;
+                changeBackground();
+                OnPropertyChanged();
+            }
+        }
+
+        public string BackgroundColor
+        {
+            get { return backgroundColor; }
+            set
+            {
+                backgroundColor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void changeBackground() {
+            string color = Color.FromRgb(Convert.ToByte(redColor), Convert.ToByte(greenColor), Convert.ToByte(blueColor)).ToString();
+            BackgroundColor = color;
+        } 
+
+        #region Propertychanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string caller = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(caller));
+        }
+        #endregion
+    }
+}
